a93361de637005870fb5ddf1a93909a910394f36
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-01-03 20:07:56 +0530

    usable app

--- script.js
+++ script.js
@@ -5,58 +5,68 @@ let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
+let masterSongName = document.getElementById("masterSongName");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
+
 let songs = [
 	{
-		songName: "Salam-e-Ishq",
+		songName: "It's Always blue : Song from legion",
 		filePath: "songs/1.mp3",
 		coverPath: "covers/1.jpg",
 	},
 	{
-		songName: "Salam-e-Ishq",
-		filePath: "songs/1.mp3",
-		coverPath: "covers/1.jpg",
+		songName: "Trap Cartel",
+		filePath: "songs/2.mp3",
+		coverPath: "covers/2.jpg",
 	},
 	{
-		songName: "Salam-e-Ishq",
-		filePath: "songs/1.mp3",
-		coverPath: "covers/1.jpg",
+		songName: "They Mad",
+		filePath: "songs/3.mp3",
+		coverPath: "covers/3.jpg",
 	},
 	{
-		songName: "Salam-e-Ishq",
-		filePath: "songs/1.mp3",
-		coverPath: "covers/1.jpg",
+		songName: "plug walk",
+		filePath: "songs/4.mp3",
+		coverPath: "covers/4.jpg",
 	},
 	{
-		songName: "Salam-e-Ishq",
-		filePath: "songs/1.mp3",
-		coverPath: "covers/1.jpg",
+		songName: "song title",
+		filePath: "songs/5.mp3",
+		coverPath: "covers/5.jpg",
 	},
 	{
-		songName: "Salam-e-Ishq",
-		filePath: "songs/1.mp3",
-		coverPath: "covers/1.jpg",
+		songName: "Sleeping at night",
+		filePath: "songs/6.mp3",
+		coverPath: "covers/6.jpg",
 	},
 	{
-		songName: "Salam-e-Ishq",
-		filePath: "songs/1.mp3",
-		coverPath: "covers/1.jpg",
+		songName: "Back it up",
+		filePath: "songs/7.mp3",
+		coverPath: "covers/7.jpg",
 	},
 	{
-		songName: "Salam-e-Ishq",
-		filePath: "songs/1.mp3",
-		coverPath: "covers/1.jpg",
+		songName: "Lady 1",
+		filePath: "songs/8.mp3",
+		coverPath: "covers/8.jpg",
 	},
 	{
-		songName: "Salam-e-Ishq",
-		filePath: "songs/1.mp3",
-		coverPath: "covers/1.jpg",
+		songName: "Lady 2",
+		filePath: "songs/9.mp3",
+		coverPath: "covers/9.jpg",
 	},
 	{
-		songName: "Salam-e-Ishq",
-		filePath: "songs/1.mp3",
-		coverPath: "covers/1.jpg",
+		songName: "True love",
+		filePath: "songs/10.mp3",
+		coverPath: "covers/10.jpg",
 	},
 ];
+
+songItems.forEach((song, i) => {
+	song.getElementsByTagName("img")[0].src = songs[i].coverPath;
+	song.getElementsByTagName("span")[0].innerText = songs[i].songName;
+
+	// song.getElementsByTagName("p")[1].innerHTML = songs[i].filePath;
+});
 
 //audioElement.play();
 
@@ -67,11 +77,13 @@ masterPlay.addEventListener("click", () => {
 		audioElement.play();
 		masterPlay.classList.remove("fa-play-circle");
 		masterPlay.classList.add("fa-pause-circle");
+
 		gif.style.opacity = 1;
 	} else {
 		masterPlay.classList.add("fa-play-circle");
 		masterPlay.classList.remove("fa-pause-circle");
 		audioElement.pause();
+		makeAllPlay();
 		gif.style.opacity = 0;
 	}
 });
@@ -86,4 +98,61 @@ audioElement.addEventListener("timeupdate", () => {
 myProgressBar.addEventListener("change", () => {
 	audioElement.currentTime =
 		(audioElement.duration * myProgressBar.value) / 100;
+});
+
+const makeAllPlay = () => {
+	Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+		(element) => {
+			element.classList.add("fa-play-circle");
+		}
+	);
+};
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach(
+	(element) => {
+		element.addEventListener("click", (e) => {
+			console.log(e);
+			makeAllPlay();
+			songIndex = parseInt(e.target.id);
+			e.target.classList.remove("fa-play-circle");
+			e.target.classList.add("fa-pause-circle");
+			audioElement.src = `songs/${songIndex + 1}.mp3`;
+			audioElement.play();
+			audioElement.currentTime = 0;
+			masterPlay.classList.remove("fa-play-circle");
+			masterPlay.classList.add("fa-pause-circle");
+			gif.style.opacity = 1;
+			masterSongName.innerText = songs[songIndex].songName;
+		});
+	}
+);
+
+document.getElementById("previous").addEventListener("click", () => {
+	if (songIndex > 0) {
+		songIndex--;
+	} else {
+		songIndex = songs.length - 1;
+	}
+	audioElement.src = `songs/${songIndex + 1}.mp3`;
+	audioElement.play();
+	audioElement.currentTime = 0;
+	masterPlay.classList.remove("fa-play-circle");
+	masterPlay.classList.add("fa-pause-circle");
+	masterSongName.innerText = songs[songIndex].songName;
+	gif.style.opacity = 1;
+});
+
+document.getElementById("next").addEventListener("click", () => {
+	if (songIndex < songs.length - 1) {
+		songIndex++;
+	} else {
+		songIndex = 0;
+	}
+	audioElement.src = `songs/${songIndex + 1}.mp3`;
+	audioElement.play();
+	audioElement.currentTime = 0;
+	masterPlay.classList.remove("fa-play-circle");
+	masterPlay.classList.add("fa-pause-circle");
+	masterSongName.innerText = songs[songIndex].songName;
+	gif.style.opacity = 1;
 });
