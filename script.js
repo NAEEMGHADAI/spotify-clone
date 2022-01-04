a2a2eab197aa77fb4c9116f79d74188c35cec29d
NAEEMGHADAI <naeemghadai007.com@gmail.com>
2022-01-04 12:12:55 +0530

    small improvements

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("Hello World");
 //Intialize the variables
 let songIndex = 0;
 let audioElement = new Audio("songs/1.mp3");
@@ -11,51 +10,61 @@ let songItems = Array.from(document.getElementsByClassName("songItem"));
 let songs = [
 	{
 		songName: "It's Always blue : Song from legion",
+		songLength: "3:50",
 		filePath: "songs/1.mp3",
 		coverPath: "covers/1.jpg",
 	},
 	{
 		songName: "Trap Cartel",
+		songLength: "2:33",
 		filePath: "songs/2.mp3",
 		coverPath: "covers/2.jpg",
 	},
 	{
 		songName: "They Mad",
+		songLength: "4:33",
 		filePath: "songs/3.mp3",
 		coverPath: "covers/3.jpg",
 	},
 	{
 		songName: "plug walk",
+		songLength: "4:27",
 		filePath: "songs/4.mp3",
 		coverPath: "covers/4.jpg",
 	},
 	{
 		songName: "song title",
+		songLength: "3:28",
 		filePath: "songs/5.mp3",
 		coverPath: "covers/5.jpg",
 	},
 	{
 		songName: "Sleeping at night",
+		songLength: "3:28",
 		filePath: "songs/6.mp3",
 		coverPath: "covers/6.jpg",
 	},
 	{
 		songName: "Back it up",
+		songLength: "4:33",
 		filePath: "songs/7.mp3",
 		coverPath: "covers/7.jpg",
 	},
 	{
 		songName: "Lady 1",
+		songLength: "3:50",
 		filePath: "songs/8.mp3",
 		coverPath: "covers/8.jpg",
 	},
 	{
 		songName: "Lady 2",
+		songLength: "3:28",
 		filePath: "songs/9.mp3",
 		coverPath: "covers/9.jpg",
 	},
 	{
 		songName: "True love",
+		songLength: "4:27",
 		filePath: "songs/10.mp3",
 		coverPath: "covers/10.jpg",
 	},
@@ -64,20 +73,18 @@ let songs = [
 songItems.forEach((song, i) => {
 	song.getElementsByTagName("img")[0].src = songs[i].coverPath;
 	song.getElementsByTagName("span")[0].innerText = songs[i].songName;
-
-	// song.getElementsByTagName("p")[1].innerHTML = songs[i].filePath;
+	song.getElementsByClassName("songLength")[0].innerText = songs[i].songLength;
 });
 
 //audioElement.play();
 
 //Handling the play button
-console.log(masterPlay);
+
 masterPlay.addEventListener("click", () => {
 	if (audioElement.paused || audioElement.currentTime <= 0) {
 		audioElement.play();
 		masterPlay.classList.remove("fa-play-circle");
 		masterPlay.classList.add("fa-pause-circle");
-
 		gif.style.opacity = 1;
 	} else {
 		masterPlay.classList.add("fa-play-circle");
@@ -111,18 +118,26 @@ const makeAllPlay = () => {
 Array.from(document.getElementsByClassName("songItemPlay")).forEach(
 	(element) => {
 		element.addEventListener("click", (e) => {
-			console.log(e);
-			makeAllPlay();
-			songIndex = parseInt(e.target.id);
-			e.target.classList.remove("fa-play-circle");
-			e.target.classList.add("fa-pause-circle");
-			audioElement.src = `songs/${songIndex + 1}.mp3`;
-			audioElement.play();
-			audioElement.currentTime = 0;
-			masterPlay.classList.remove("fa-play-circle");
-			masterPlay.classList.add("fa-pause-circle");
-			gif.style.opacity = 1;
-			masterSongName.innerText = songs[songIndex].songName;
+			if (e.target.classList.contains("fa-pause-circle")) {
+				e.target.classList.remove("fa-pause-circle");
+				e.target.classList.add("fa-play-circle");
+				masterPlay.classList.add("fa-play-circle");
+				masterPlay.classList.remove("fa-pause-circle");
+				audioElement.pause();
+				gif.style.opacity = 0;
+			} else {
+				makeAllPlay();
+				songIndex = parseInt(e.target.id);
+				e.target.classList.remove("fa-play-circle");
+				e.target.classList.add("fa-pause-circle");
+				audioElement.src = `songs/${songIndex + 1}.mp3`;
+				audioElement.play();
+				audioElement.currentTime = 0;
+				masterPlay.classList.remove("fa-play-circle");
+				masterPlay.classList.add("fa-pause-circle");
+				gif.style.opacity = 1;
+				masterSongName.innerText = songs[songIndex].songName;
+			}
 		});
 	}
 );
